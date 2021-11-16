971881c5bd843ca0f0df46f3f72d6887da730c5e
g0709-19 <sch030518@gmail.com>
2021-11-17 04:26:34 +0900

    회원가입 UI 작성 전 기반 구성

---
+++ qrless/src/styles/JoinStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const joinStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        padding: 10,
+        fontSize: 26,
+        fontWeight: '600',
+        color: 'black',
+    },
+    error: {
+        fontWeight: '400',
+        color: 'red',
+    },
+});
